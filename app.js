40f4fabcd634ad52cdef3811797da12a2b95d124
Gustavo Calcavara <gcalcavara@gmail.com>
2020-05-26 14:59:18 -0300

    tetrominoes dropping down complete

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ insertGridUnits(gameGrid);
 
 let squares = Array.from(document.querySelectorAll(".game-grid div"));
 
-console.log(squares);
+function identifyLastLine() {
+    for (let i = 0; i < gridWidth; i++) {
+        squares[gridSize - 1 - i].classList.add("last-line");
+    }
+}
+
+identifyLastLine();
 
 // Tetrominoes Forms
 
@@ -53,3 +59,49 @@ const iTetromino = [
     [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 3 + 1],
     [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth + 3]
 ];
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+let currentPosition = 4;
+let currentRotation = 0;
+
+// Randomly select a Tetromino and its first rotation
+let random = Math.floor(Math.random() * theTetrominoes.length);
+let current = theTetrominoes[random][currentRotation];
+
+// Draw the Tetromino
+function draw() {
+    current.forEach(gridPos => {
+        squares[currentPosition + gridPos].classList.add("tetromino");
+    })
+}
+
+// Undraw the Tetromino
+function undraw() {
+    current.forEach(gridPos => {
+        squares[currentPosition + gridPos].classList.remove("tetromino");
+    })
+}
+
+// Make the Tetromino move down every second
+const timerId = setInterval(moveDown, 100);
+
+// Move down function
+function moveDown() {
+    undraw();
+    currentPosition += gridWidth;
+    draw();
+    freeze();
+}
+
+// A function to check the condition to stop the Tetromino going down
+function freeze() {
+    if (current.some(gridPos => (squares[gridPos + currentPosition].classList.contains("last-line") || squares[gridPos + currentPosition + gridWidth].classList.contains("taken")))) {
+        current.forEach(gridPos => squares[gridPos + currentPosition].classList.add("taken"));
+        // Start a new Tetromino
+        currentPosition = 4;
+        random = Math.floor(Math.random() * theTetrominoes.length);
+        current = theTetrominoes[random][currentRotation];
+        draw();
+    }
+}
